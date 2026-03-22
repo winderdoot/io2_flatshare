98692212469713a4f1579faeda42c7e0611fe9b1
Mateusz <mateuszlerke123@gmail.com>
2026-03-22 18:16:11 +0100

    better navbar

--- flatshare_frotend/src/layouts/MainLayout.tsx
+++ flatshare_frotend/src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <>
         <Navbar />
-        <Outlet />
+        <main style={{paddingTop: "var(--navbar-height)"}}>
+          <Outlet />
+        </main>
     </>
   );
 };
