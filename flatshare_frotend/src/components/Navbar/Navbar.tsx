74128f7e208e39601aaae9115706e87df30b56de
Svetlana Gridina <svetagridina122@gmail.com>
2026-04-06 18:06:34 +0200

    frontend: change location of int. buttons

--- flatshare_frotend/src/components/Navbar/Navbar.tsx
+++ flatshare_frotend/src/components/Navbar/Navbar.tsx
@@ -11,13 +11,14 @@ const Navbar = () => {
       <div className={styles.inner}>
         <div className={styles.logo}>FlatShare</div>
 
-        <div className={styles.right}>
+        <div className={styles.links}>
+          <Link to="/">{t("nav.home")}</Link>
+          <Link to="/color-palette">{t("nav.colors")}</Link>
+          <Link to="/login">{t("nav.account")}</Link>
+        </div>
+
+        <div className={styles.locale}>
           <LocaleSwitcher />
-          <div className={styles.links}>
-            <Link to="/">{t("nav.home")}</Link>
-            <Link to="/color-palette">{t("nav.colors")}</Link>
-            <Link to="/login">{t("nav.account")}</Link>
-          </div>
         </div>
       </div>
     </nav>
