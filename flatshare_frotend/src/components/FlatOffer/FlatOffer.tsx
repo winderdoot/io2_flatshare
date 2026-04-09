7f8662e8e5b5dcd9073252b4069c2ed30318df9e
Mateusz <mateuszlerke123@gmail.com>
2026-04-09 22:23:56 +0200

    offer: better offer component

--- flatshare_frotend/src/components/FlatOffer/FlatOffer.tsx
+++ flatshare_frotend/src/components/FlatOffer/FlatOffer.tsx
@@ -1,19 +1,41 @@
 import "./FlatOffer.css";
+import { FlatOfferProps } from "./FlatOfferProps";
 
-const FlatOffer = () => {
+const FlatOffer = ({
+    vertical = true,
+    title,
+    description,
+    phone,
+    location,
+    price,
+    currency,
+    image
+}: FlatOfferProps) => {
     return (
-        <>
-
-            <div className="offer-container">
-                <img src="src/assets/rent_house.png" />
-                <div className="offer-info">
-                    <h2>Flat</h2>
-                    <p>Address</p>
-                    <p>Price</p>
+        <div className={`offer-card ${vertical ? "vertical" : "horizontal"}`}>
+            <div className="offer-image-wrapper">
+                <img src={image} alt={title} className="offer-image" />
+                <div className="offer-price">
+                    {price} {currency}
                 </div>
             </div>
-        </>
-    )
-}
 
-export default FlatOffer
+            <div className="offer-content">
+                <h2 className="offer-title">{title}</h2>
+
+                <p className="offer-location">
+                    📍 {location.street}, {location.aptNumber}, {location.district}, {location.city}
+                </p>
+
+                <p className="offer-description">{description}</p>
+
+                <div className="offer-footer">
+                    <span className="offer-phone">📞 {phone}</span>
+                    <button className="contact-button">Kontakt</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FlatOffer;
