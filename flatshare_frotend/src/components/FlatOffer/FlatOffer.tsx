12655ed54b567a715153d9d57c7718d1d8a52130
Mateusz <mateuszlerke123@gmail.com>
2026-04-09 22:55:42 +0200

    flatoffer: area information added

--- flatshare_frotend/src/components/FlatOffer/FlatOffer.tsx
+++ flatshare_frotend/src/components/FlatOffer/FlatOffer.tsx
@@ -8,6 +8,7 @@ const FlatOffer = ({
     phone,
     location,
     price,
+    area, // TODO: add area info
     currency,
     image
 }: FlatOfferProps) => {
@@ -23,15 +24,21 @@ const FlatOffer = ({
             <div className="offer-content">
                 <h2 className="offer-title">{title}</h2>
 
-                <p className="offer-location">
-                    📍 {location.street}, {location.aptNumber}, {location.district}, {location.city}
-                </p>
+                <div className="offer-mini-info">
+                    <p className="offer-location">
+                        📍 {location.street}, {location.aptNumber}, {location.district}, {location.city}
+                    </p>
+
+                    <p className="offer-area">
+                        📐 {area} m<sup>2</sup>
+                    </p>
+                </div>
 
                 <p className="offer-description">{description}</p>
 
                 <div className="offer-footer">
                     <span className="offer-phone">📞 {phone}</span>
-                    <button className="contact-button">Kontakt</button>
+                    <button className="contact-button">Więcej</button>
                 </div>
             </div>
         </div>
