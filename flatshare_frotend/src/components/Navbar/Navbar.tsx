f17eb4b7684c284fa533e18772d2c31683ffa715
Mateusz <mateuszlerke123@gmail.com>
2026-04-11 22:50:01 +0200

    login - happy path done

--- flatshare_frotend/src/components/Navbar/Navbar.tsx
+++ flatshare_frotend/src/components/Navbar/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <Link to="/">{t("nav.home")}</Link>
           <Link to="/color-palette">{t("nav.colors")}</Link>
           <Link to="/offer">{t("nav.offer")}</Link>
-          { user && <Link to="/" onClick={handleLogout}>{t("nav.log_out")} {user}</Link>}
+          { user && <Link to="/" onClick={handleLogout}>{t("nav.log_out")} {user.firstName}</Link>}
           { !user && <Link to="/login">{t("nav.account")}</Link>}
         </div>
 
