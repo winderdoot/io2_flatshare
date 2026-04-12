a5b782fdfc41e4e620adc4985a298654f8d8e3dc
Marek Rauchfleisz <01189160@pw.edu.pl>
2026-04-12 13:14:05 +0200

    S2/flatshare_server: update database

---
+++ flatshare_server/flatshare_server/Migrations/20260412111257_AddTenantPreferences.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace flatshare_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenantPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TenantPreferences_Currency",
+                table: "UserRole",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TenantPreferences_MaxPrice",
+                table: "UserRole",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TenantPreferences_PetsAllowed",
+                table: "UserRole",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "TenantPreferences_PreferredDistricts",
+                table: "UserRole",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TenantPreferences_SmokingAllowed",
+                table: "UserRole",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantPreferences_Currency",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "TenantPreferences_MaxPrice",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "TenantPreferences_PetsAllowed",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "TenantPreferences_PreferredDistricts",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "TenantPreferences_SmokingAllowed",
+                table: "UserRole");
+        }
+    }
+}
