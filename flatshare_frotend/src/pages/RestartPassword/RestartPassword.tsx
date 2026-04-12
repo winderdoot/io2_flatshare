c1eaa9c5cf5b7e044e956e5d269cdf4b93e698c3
Mateusz <mateuszlerke123@gmail.com>
2026-04-12 19:43:24 +0200

    password reset - UI done

--- flatshare_frotend/src/pages/RestartPassword/RestartPassword.tsx
+++ flatshare_frotend/src/pages/RestartPassword/RestartPassword.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { CustomTextInput } from "../../components/CustomTextInput/CustomTextInput";
 import "./RestartPassword.css";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../config";
 
 
 export const RestartPassword = () => {      
@@ -11,14 +12,26 @@ export const RestartPassword = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [sent, setSent] = useState(false);
 
-  const handleSubmitCodeSent = async () => {
-    alert(t("resetPassword.emailSent"));
+  const [token, setToken] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
 
-    // TODO send email and code verification
+  const handleSubmitCodeSent = async () => {   
+    setSent(false);
+
     setLoading(true);
-    setError(null);
+    const  res = await fetch(`${API_URL}/api/v1/auth/password-reset/request`,{method: "POST", body: JSON.stringify({email}), headers: {"Content-Type": "application/json", "Accept": "application/json",}});   
+    console.log(res);
     setLoading(false);
+
+    setSent(true);
+    alert(t("resetPassword.emailSent"));
+  };
+
+  const handleSubmitNewPassword = async () => {
+    alert("password changed");
   };
 
   return (
@@ -34,21 +47,56 @@ export const RestartPassword = () => {
             onChange={setEmail}
           />
 
-
+          {sent && <div className="password-reset-container">
+              <CustomTextInput
+                label={t("resetPassword.token")}
+                placeholder={t("resetPassword.tokenPlaceholder")}
+                value={token}
+                onChange={setToken}
+              />
+              <CustomTextInput
+                label={t("resetPassword.passwordLabel")}
+                placeholder={t("resetPassword.passwordPlaceholder")}
+                value={password}
+                onChange={setPassword}
+              />
+              <CustomTextInput
+                label={t("resetPassword.confirmPasswordLabel")}
+                placeholder={t("resetPassword.confirmPasswordPlaceholder")}
+                value={confirmPassword}
+                onChange={setConfirmPassword}
+              />
+            </div>
+          }
         </div>
 
-        <div className="buttons-container">
+        {!sent && <div className="buttons-container">
+          
           <button onClick={handleSubmitCodeSent} disabled={loading}>
             {loading ? t("resetPassword.submitLoading") : t("resetPassword.submit")}
-          </button>
-
-          {error && <p className="error-message">{error}</p>}
+          </button>         
 
           <label>
             {t("resetPassword.hasAccount")}{" "}
             <Link to="/login">{t("resetPassword.loginLink")}</Link>
           </label>
-        </div>
+        </div>}    
+
+        {sent && <div className="buttons-container">
+          {sent &&  <button onClick={handleSubmitNewPassword} disabled={loading}>
+              {loading ? t("resetPassword.submitPasswordLoading") : t("resetPassword.submitPassword")}
+            </button>}
+
+          {error && <p className="error-message">{error}</p>} 
+
+          <label>
+            {t("resetPassword.problem")}{". "}
+          <span onClick={handleSubmitCodeSent} className="link">
+            {t("resetPassword.resent")}
+          </span>
+          </label>
+        </div>}
+
       </div>
     </div>
   );
