1953ff4926d3aeb222477aea7e2e45bc48b7d2f8
Mateusz <mateuszlerke123@gmail.com>
2026-05-03 19:48:23 +0200

    offers works a bit

--- flatshare_frotend/src/components/OffersList/OffersList.tsx
+++ flatshare_frotend/src/components/OffersList/OffersList.tsx
@@ -3,13 +3,13 @@ import { useListings } from "./useListings";
 export default function Listings() {
   const { data, isLoading, isError, isFetching } = useListings();
 
+  console.log(data);
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Błąd</div>;
 
   return (
     <div>
-      {data as string}
-
       {isFetching && <div>Odświeżanie...</div>}
     </div>
   );
