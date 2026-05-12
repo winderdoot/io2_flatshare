f4cb5a8939da7d23133469af040c3dc0a5c060b0
winderdoot <01189157@pw.edu.pl>
2026-05-12 17:14:43 +0200

    S4/server: db migration - AddBookings

---
+++ flatshare_server/flatshare_server/Migrations/20260512145428_AddBookings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace flatshare_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ListingId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    TotalPrice_Value = table.Column<decimal>(type: "numeric", nullable: false),
+                    TotalPrice_Curr = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookingId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_ListingId",
+                table: "Bookings",
+                column: "ListingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_TenantId",
+                table: "Bookings",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
