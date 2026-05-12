4707e9b3e76ab95fda349337ca1a1446a731e3b5
winderdoot <01189157@pw.edu.pl>
2026-05-12 23:41:24 +0200

    S4/server: fix docker-compose stripe-cli entrypoint magic script

---
+++ flatshare_server/flatshare_server/Infrastructure/Configuration/StripeWebhookSecret.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace flatshare_server.Infrastructure.Configuration;
+
+public class StripeWebhookSecretProvider
+{
+    public required string Secret { get; set; }
+}
---
+++ flatshare_server/flatshare_server/Infrastructure/Services/Bookings/WebhookService.cs
@@ -0,0 +1,30 @@
+﻿using flatshare_server.Infrastructure.Configuration;
+using flatshare_server.Infrastructure.Model.Responses;
+using Microsoft.Extensions.Options;
+
+namespace flatshare_server.Infrastructure.Services.Bookings;
+
+public record WebhookInitRequest(string Secret);
+
+public class WebhookService
+(
+    StripeWebhookSecretProvider secretProvider,
+    IWebHostEnvironment env,
+    IOptions<StripeOptions> stripeOpts
+)
+{
+    public void InitializeSecret(string secret, string initKey)
+    {
+        if (!env.IsDevelopment())
+            throw ErrorResponse.Generate("Only needed in local development. In production stripe servers can access deployed application.", StatusCodes.Status403Forbidden);
+
+        var expected = stripeOpts.Value.WebhookSecretInitKey;
+
+        if (string.IsNullOrEmpty(initKey) || initKey != expected)
+        {
+            throw ErrorResponse.Generate("Invalid Initialization Key.", StatusCodes.Status401Unauthorized);
+        }
+
+        secretProvider.Secret = secret;
+    }
+}
